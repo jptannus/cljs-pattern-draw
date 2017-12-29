0715b52933723284daf6c5ec1fe73c8470a6ee25
João Paulo <jptannus@gmail.com>
2017-12-29 05:39:08 +0000

    Add unit test for matrix manipulations

--- test/pattern/utils-test.cljs
+++ test/pattern/utils-test.cljs
@@ -1,5 +1,24 @@
 (ns pattern.utils-test
-  (:require [cljs.test :refer-macros [deftest is testing]]))
+  (:require
+    [cljs.test :refer-macros [deftest is testing]]
+    [pattern.utils :as u]))
 
-(deftest adding
-  (is (= 3 (+ 1 1))))
+(deftest duplicate-matrix-horizontal
+  (let [matrix [[1 1][1 1]]
+        matrix-result [[1 1 1 1][1 1 1 1]]]
+    (is (= matrix-result (u/duplicate-matrix-horizontal matrix)))))
+
+(deftest quadriplicate-matrix-horizontal
+  (let [matrix [[1 1][1 1]]
+        matrix-result [[1 1 1 1 1 1 1 1][1 1 1 1 1 1 1 1]]]
+    (is (= matrix-result (u/duplicate-matrix-horizontal matrix 4)))))
+
+(deftest duplicate-matrix-vertical
+  (let [matrix [[1 1][1 1]]
+        matrix-result [[1 1][1 1][1 1][1 1]]]
+    (is (= matrix-result (u/duplicate-matrix-vertical matrix)))))
+
+(deftest quadriplicate-matrix-vertical
+  (let [matrix [[1 1][1 1]]
+        matrix-result [[1 1][1 1][1 1][1 1][1 1][1 1][1 1][1 1]]]
+    (is (= matrix-result (u/duplicate-matrix-vertical matrix 4)))))
